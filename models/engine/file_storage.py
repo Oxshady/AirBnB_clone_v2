860b7500c8fd68f01715ebeeca523592d9b0217b
Oxshady <shadymahmod55@gmail.com>
2024-07-29 19:04:08 +0300

    update add and adding delete puplic instance method

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,8 +8,15 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
+        if cls:
+            tmp = {}
+            for key, value in FileStorage.__objects.items():
+                k = key.split(".")
+                if k[0] == cls.__name__:
+                    tmp.update({key: value})
+            return tmp
         return FileStorage.__objects
 
     def new(self, obj):
@@ -24,6 +31,14 @@ class FileStorage:
             for key, val in temp.items():
                 temp[key] = val.to_dict()
             json.dump(temp, f)
+
+    def delete(self, obj=None):
+        """delet given object"""
+        if obj:
+            cname = obj.__class__.__name__
+            k = f"{cname}.{obj.id}"
+            if k in self.all().keys():
+                del self.all()[k]
 
     def reload(self):
         """Loads storage dictionary from file"""
@@ -45,6 +60,6 @@ class FileStorage:
             with open(FileStorage.__file_path, 'r') as f:
                 temp = json.load(f)
                 for key, val in temp.items():
-                        self.all()[key] = classes[val['__class__']](**val)
+                    self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
