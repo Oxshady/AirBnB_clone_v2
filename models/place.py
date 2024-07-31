c437b93c14b7649a34278bd6af0462bfd5dc2e0b
Oxshady <shadymahmod55@gmail.com>
2024-07-31 05:31:45 +0300

    mapping place

--- models/place.py
+++ models/place.py
@@ -1,18 +1,20 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
-from models.base_model import BaseModel
-
-
-class Place(BaseModel):
+from models.base_model import BaseModel, Base
+from sqlalchemy import String, ForeignKey, Integer, Float
+from sqlalchemy.orm import Mapped, mapped_column
+from typing import Optional
+class Place(BaseModel, Base):
     """ A place to stay """
-    city_id = ""
-    user_id = ""
-    name = ""
-    description = ""
-    number_rooms = 0
-    number_bathrooms = 0
-    max_guest = 0
-    price_by_night = 0
-    latitude = 0.0
-    longitude = 0.0
+    __tablename__ = "places"
+    city_id: Mapped[str] = mapped_column(String(60), ForeignKey('cities.id'), nullable=False)
+    user_id: Mapped[str] = mapped_column(String(128), ForeignKey('users.id'), nullable=False)
+    name: Mapped[str] = mapped_column(String(128), nullable=False)
+    description: Mapped[Optional[str]] = mapped_column(String(1024), nullable=True)
+    number_rooms: Mapped[int] = mapped_column(Integer, nullable=False, default=0)
+    number_bathrooms: Mapped[int] = mapped_column(Integer, nullable=False, default=0)
+    max_guest: Mapped[int] = mapped_column(Integer, nullable=False, default=0)
+    price_by_night: Mapped[int] = mapped_column(Integer, nullable=False, default=0)
+    latitude: Mapped[Optional[float]] = mapped_column(Float, nullable=True)
+    longitude: Mapped[Optional[float]] = mapped_column(Float, nullable=True)
     amenity_ids = []
