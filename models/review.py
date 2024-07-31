36b5c01fe4ea98a6225277023be0a534c816721b
Oxshady <shadymahmod55@gmail.com>
2024-07-31 05:56:51 +0300

    establishing relatioship between entities

--- models/review.py
+++ models/review.py
@@ -1,10 +1,30 @@
 #!/usr/bin/python3
 """ Review module for the HBNB project """
 from models.base_model import BaseModel
+from sqlalchemy import (
+    String,
+    ForeignKey
+    )
+from sqlalchemy.orm import (
+    Mapped,
+    mapped_column
+)
 
 
 class Review(BaseModel):
     """ Review classto store review information """
-    place_id = ""
-    user_id = ""
-    text = ""
+    __tablename__ = "reviews"
+    text: Mapped[str] = mapped_column(
+        String(1024),
+        nullable=False
+    )
+    place_id: Mapped[str] = mapped_column(
+        String(60),
+        ForeignKey('places.id'),
+        nullable=False
+    )
+    user_id: Mapped[str] = mapped_column(
+        String(60),
+        ForeignKey('users.id'),
+        nullable=False
+    )
