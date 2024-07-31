5c7f615178608d5df89e7ac9b68c741177e0198a
Oxshady <shadymahmod55@gmail.com>
2024-07-31 03:12:45 +0300

    ^_^

--- models/city.py
+++ models/city.py
@@ -2,10 +2,19 @@
 """ City Module for HBNB project """
 from models.base_model import BaseModel
 from models.base_model import Base
-from sqlalchemy.orm import  Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column
 from sqlalchemy import String, ForeignKey
+
+
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
-    __tablename__="cities"
-    name: Mapped[str] = mapped_column(String(128), nullable=False)
-    state_id: Mapped[str] = mapped_column(String(60), ForeignKey('states.id'), nullable=False)
+    __tablename__ = "cities"
+    name: Mapped[str] = mapped_column(
+        String(128),
+        nullable=False
+        )
+    state_id: Mapped[str] = mapped_column(
+        String(60),
+        ForeignKey('states.id'),
+        nullable=False
+        )
