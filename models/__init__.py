1988ac70b3e87b2811ea3b024853049b3ceb20f6
Oxshady <shadymahmod55@gmail.com>
2024-07-31 20:08:25 +0300

    handle import *

--- models/__init__.py
+++ models/__init__.py
@@ -1,13 +1,22 @@
 #!/usr/bin/python3
 """This module instantiates an object of class FileStorage"""
+from models.base_model import BaseModel
+from models.city import City
+from models.state import State
+from models.user import User
+from models.place import Place
+from models.amenity import Amenity
+
 from os import getenv
+
 
 type = getenv('HBNB_TYPE_STORAGE')
 if type == "db":
     from models.engine.db_storage import DBStorage
     storage = DBStorage()
-    storage.reload()
 else:
     from models.engine.file_storage import FileStorage
     storage = FileStorage()
-    storage.reload()
+
+storage.reload()
+
