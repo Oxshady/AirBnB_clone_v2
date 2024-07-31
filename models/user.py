f51fcbfd5aa736dc6ed4b345471097a661f78e8c
Oxshady <shadymahmod55@gmail.com>
2024-07-31 05:31:59 +0300

    mapping user

--- models/user.py
+++ models/user.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-from typing import Optional
+from typing import Optional, List
 from models.base_model import BaseModel, Base
 from sqlalchemy import String
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import (
+    Mapped,
+    mapped_column,
+    relationship
+    )
 
 
 class User(BaseModel, Base):
@@ -13,3 +17,9 @@ class User(BaseModel, Base):
     password: Mapped[str] = mapped_column(String(128), nullable=False)
     first_name: Mapped[Optional[str]] = mapped_column(String(128), nullable=True)
     last_name: Mapped[Optional[str]] = mapped_column(String(128), nullable=True)
+    from models.place import Place
+    places: Mapped[List['Place']] = relationship(
+        Place,
+        backref="user",
+        cascade='all, delete-orphan'
+        )
