3de4f7ddde38a0c7e0b276ffcc168ef227ae6ac0
Oxshady <shadymahmod55@gmail.com>
2024-08-03 05:21:26 +0300

    configure my servers ^_^

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+sudo apt update -y -qq > /dev/null
+sudo apt install nginx -y -qq > /dev/null
+my_app="/data/web_static/releases/test/"
+if [ ! -d $my_app ]; then
+	sudo mkdir -p $my_app
+fi
+if [ ! -f "$my_app/index.html" ]; then
+	echo "fucl"
+	sudo touch "$my_app/index.html"
+	sudo chmod 755 "$my_app/index.html"
+fi
+data="<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>"
+sudo echo "$data" | sudo tee "$my_app/index.html" > /dev/null
+sym_lin="/data/web_static/current"
+dir_to_lin="/data/web_static/releases/test/"
+sudo ln -sf "$dir_to_lin" "$sym_lin"
+sudo chown -R "ubuntu":"ubuntu" data/
+sudo sed -i '59i\\troot /data/web_static/current/;' /etc/nginx/sites-available/default
+sudo sed -i '60i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+sudo service nginx start
